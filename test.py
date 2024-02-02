5f9d8675b29d075496d7f7a213af495195f62bc4
Igor Kirpichnikov <kirp777@yandex.ru>
2024-02-02 20:11:00 +0400

    nado

---
+++ test.py
@@ -0,0 +1,48 @@
+courses = {
+    'Python-разработчик': {
+        'Стек': {
+            'Python',
+            'Django',
+            'GitHub',
+            'SQL',
+            'PostgreSQL',
+            'Bash',
+            'Nginx',
+            'Gunicorn',
+            'Docker',
+            'Docker Hub'
+        },
+        'Продолжительность': '9 месяцев'
+    },
+    'Python-разработчик плюс': {
+        'Стек': {
+            'Python',
+            'Django',
+            'GitHub',
+            'SQL',
+            'PostgreSQL',
+            'Bash',
+            'Nginx',
+            'Gunicorn',
+            'Docker',
+            'Docker Hub',
+            'Flask',
+            'FastAPI',
+            'Scrapy',
+            'Selenium',
+            'SQLAlchemy'
+        },
+        'Продолжительность': '14 месяцев'
+    }
+}
+
+diff = set.difference(
+    courses['Python-разработчик плюс']['Стек'],
+    courses['Python-разработчик']['Стек']
+)
+courses_diff = str.join(', ', diff)
+
+print(
+    'Отличия стеков в курсах «Python-разработчик плюс» и '
+    f'«Python-разработчик»: {courses_diff}'
+)
